2009b1678319f18b8875b1826a101e634f4ee623
Evan Merritt <109871289+MrMerrittx702@users.noreply.github.com>
2023-10-26 10:34:46 -0500

    Update WhileLoops.java

--- src/main/java/WhileLoops.java
+++ src/main/java/WhileLoops.java
@@ -3,59 +3,67 @@
 public class WhileLoops {
   //USE WHILE LOOPS ONLY
   
-  /**1) Implement the following method to count up from 1 to 10 on the same line with a space in between.*/
+  /**1) Implement the following method to count up from 1 to 10 on the same line with a space in between.
+  * The last number should not have a space after it. */
   public static void countUp() {
 
   }
   
-  /**2) Implement the following method from 10 to 1 on the same line with a space in between.*/
-  static void countDown() {
+  /**2) Implement the following method from 10 to 1 on the same line with a space in between.
+  * The last number should not have a space after it.*/
+  public static void countDown() {
 
   }
   
-  /**3) Implement the following method to count to 30 by 3s from 0 on the same line with a space in between.*/
-  static void countUpMultiples() {
+  /**3) Implement the following method to count to 30 by 3s from 0 on the same line with a space in between.
+  * The last number should not have a space after it.*/
+  public static void countUpMultiples() {
 
   }
   
   /** 4) Implement the following method to count down from 20 to 0 by 5s on the same line with a space in between. */
-  static void countDownMultiples() {
+  public static void countDownMultiples() {
 
   }
   
   /** 5) Implement the following method to count to 5, 3 times without duplicating your loop code 
-   * Your output should on the same line with a space in between each number.*/
-  static void nestedLoop() {
+   * Your output should on the same line with a space in between each number.
+   * The last number should have a space after it.*/
+  public static void nestedLoop() {
 
   }
   
   
-  /**6) Implement the following method to print out each individual character of the string with a space in between.*/
-  static void printChars(String str) {
+  /**6) Implement the following method to print out each individual character of the string with a space in between.
+  * The last character should not have a space after it.*/
+  public static void printChars(String str) {
 
   }
   
   /**7) Implement the following method using a do while loop print out the string while keepGoing is true
    * If keepGoing is initialized to false the method should execute at least once. */
-  static void doWhile(String str, boolean keepGoing) {
+  public static void doWhile(String str, boolean keepGoing) {
 
   }
   
   /**8) Implement the following method to loop from 0 to max by 1s, but break if count is divisible by 7
-   * Your output should on the same line with a space in between each number*/
-  static void breakCount(int max){
+   * Your output should on the same line with a space in between each number
+   * The last number should have a space after it.*/
+  public static void breakCount(int max){
 
   }
   
   /**9) Implement the following method to count up from 0 to 16 by 1s but continue if count is even
-   * Your output should on the same line with a space in between each number*/
-  static void skipEvens() {
+   * Your output should on the same line with a space in between each number
+   * The last number should have a space after it.*/
+  public static void skipEvens() {
 
   }
   
   /**10) Implement the following method to iterate through the array and print out each value*
-   * Your output should on the same line with a space in between each number*/
-  static void printArray(String[] array) {
+   * Your output should on the same line with a space in between each character
+   * The last character should not have a space after it.*/
+  public static void printArray(String[] array) {
   
   }
   
